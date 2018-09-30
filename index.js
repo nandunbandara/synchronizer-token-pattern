813d78efd8b659e3e6da15c39cce00a1dd05df67
Nandun Thilina Bandara <ntbandara3@hotmail.com>
2018-10-01 02:15:55 +0530

    Implemented login and token generation

--- index.js
+++ index.js
@@ -1,6 +1,10 @@
 const 
     express = require('express'),
     cookieParser = require('cookie-parser'),
+    randomBytes = require('random-bytes'),
+    bodyParser = require('body-parser'),
+    
+    constants = require('./app/constants'),
     
     app = express(),
 
@@ -9,9 +13,64 @@ const
 // middleware
 // TODO: set middleware in app/middleware.js
 app.use(cookieParser());
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.json());
+app.use(express.static('public'));
 
+const SESSION_DATA = {};
 
-// TODO: add routes
+// default route : check if a session is active
+app.get('/', (req, res) => {
+
+    let session_id = req.cookies['session-id'];
+
+    if(session_id && SESSION_DATA[session_id]){
+        res.sendFile('public/form.html', { root: __dirname });
+    } else {
+        res.sendFile('public/login.html', { root: __dirname});
+    }
+    
+});
+
+// handle user login and token generation
+app.post('/login', (req, res) => {
+
+    let username = req.body.username;
+    let password = req.body.password;
+
+    // validate user input
+    if(username === undefined || username === ""){
+        res.status(400).json({ success:false, message: "Username undefined"});
+        return;
+    }
+
+    if(password === undefined || password === ""){
+        res.status(400).json({ success:false, message: "Password undefined"});
+        return;
+    }
+
+    if(username === constants.username && password === constants.password){
+
+        // generate session info
+        let session_id = Buffer.from(randomBytes.sync(32)).toString('base64');
+        let csrf_token = Buffer.from(randomBytes.sync(32)).toString('base64');
+
+        // saving session info
+        SESSION_DATA[session_id] = csrf_token;
+
+        res.setHeader('Set-Cookie', [`session-id=${session_id}`, `time=${Date.now()}`]);
+        res.sendFile('public/form.html', { root: __dirname });
+    }else {
+        res.status(405).json({ success:false, message:"Unauthorized user"});
+        res.redirect('/');
+    }
+})
+
+app.post('/post', (req, res) => {
+    
+    
+
+});
 
 app.listen(PORT, err => {
     if(err){
